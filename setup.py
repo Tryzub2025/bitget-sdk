67535ace0c5a4ab610c8644710b6fe08db8fba43
Tryzub2025 <ctreyder@gmail.com>
2025-04-12 16:21:30 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bitget_sdk",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Bitget SDK for Python",
+    author="Bitget",
+)
